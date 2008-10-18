2536665dfbabe6d37050a263026f0d0a620d5ee2
catlin.sean <catlin.sean@45cd278c-9cb8-11dd-87e9-a30ea5446288>
2008-10-18 05:19:50 +0000

    first 0.0.2 alpha release

--- Manilla2D-DevTools-DisableM2D/Manilla2D-DevTools-DisableM2D.cpp
+++ Manilla2D-DevTools-DisableM2D/Manilla2D-DevTools-DisableM2D.cpp
@@ -71,8 +71,82 @@
 #define new DEBUG_NEW
 #endif
 
+void DisableM2D();
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
+    DisableM2D();
 
     return FALSE;
+}
+
+
+void DisableM2D()
+{
+    //MessageBox(NULL, TEXT("Disable M2D"), TEXT("Disable M2D"), MB_OK);
+
+    HKEY mainHKey;
+    DWORD enabledState = 0;
+    TCHAR mainKeyName[MAX_PATH];
+    TCHAR subKeyName[MAX_PATH];
+    TCHAR valueName[MAX_PATH];
+
+    // backup the date enabled state
+    wsprintf(mainKeyName, TEXT("\\Software\\Microsoft\\Today"));
+    
+    if(RegOpenKeyEx(HKEY_LOCAL_MACHINE, mainKeyName, 0, 0, &mainHKey) == ERROR_SUCCESS)
+    {
+        wsprintf(valueName, TEXT("Date"));
+        enabledState = TRUE;
+        RegSetValueEx(mainHKey, valueName, NULL, REG_DWORD, (CONST BYTE*)&enabledState, sizeof(DWORD));
+        RegFlushKey(mainHKey);
+        RegCloseKey(mainHKey);
+    }
+
+    // enumerate the sub items of the today screen
+    wsprintf(mainKeyName, TEXT("\\Software\\Microsoft\\Today\\Items"));
+
+    if(RegOpenKeyEx(HKEY_LOCAL_MACHINE, mainKeyName, 0, 0, &mainHKey) == ERROR_SUCCESS)
+    {
+        HKEY subHKey;
+        DWORD subKeyIndex = 0;
+        DWORD subKeyBufferSize = MAX_PATH;
+        TCHAR subKeyBuffer[MAX_PATH];
+
+        LONG retVal = RegEnumKeyEx(mainHKey, subKeyIndex, subKeyBuffer, &subKeyBufferSize, NULL, NULL, NULL, NULL);
+
+        while(retVal == ERROR_SUCCESS)
+        {
+            enabledState = 0;
+
+            wsprintf(subKeyName, TEXT("\\Software\\Microsoft\\Today\\Items\\%s"), subKeyBuffer);
+
+            if(RegOpenKeyEx(HKEY_LOCAL_MACHINE, subKeyName, 0, 0, &subHKey) == ERROR_SUCCESS)
+            {
+                wsprintf(valueName, TEXT("Enabled"));
+
+                if(_tcscmp(subKeyBuffer, TEXT("EnableM2D-DevTool")) == 0)
+                {
+                    enabledState = TRUE;
+                }
+                else
+                {
+                    enabledState = FALSE;
+                }
+
+                RegSetValueEx(subHKey, valueName, NULL, REG_DWORD, (CONST BYTE*)&enabledState, sizeof(DWORD));
+                RegFlushKey(subHKey);
+                RegCloseKey(subHKey);
+            }
+
+            subKeyIndex++;
+            subKeyBufferSize = MAX_PATH;
+            retVal = RegEnumKeyEx(mainHKey, subKeyIndex, subKeyBuffer, &subKeyBufferSize, NULL, NULL, NULL, NULL);
+        }
+
+        RegFlushKey(mainHKey);
+        RegCloseKey(mainHKey);
+    }
+
+    ::SendMessage(HWND_BROADCAST, WM_WININICHANGE, 0xF2, 0);
 }
