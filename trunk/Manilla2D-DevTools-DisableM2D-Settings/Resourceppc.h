083d859544725f723ebc3a7667d2e3149eb0e65c
catlin.sean <catlin.sean@45cd278c-9cb8-11dd-87e9-a30ea5446288>
2008-10-21 03:43:55 +0000

    got the softkeys setup and got the "settings dialog" look to it... just need to add the list control and functions to deal with that

--- trunk/Manilla2D-DevTools-DisableM2D-Settings/Resourceppc.h
+++ trunk/Manilla2D-DevTools-DisableM2D-Settings/Resourceppc.h
@@ -1,20 +1,18 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Manilla2D-DevTools-DisableM2D-Settingsppc.RC
+// Used by Manilla2D-DevTools-DisableM2D-Settingsppc.rc
 //
-#define IDR_MAINFRAME			128
-#define IDD_MANILLA2DDEVTOOLSDISABLEM2DSETTINGS_DIALOG			102
-#define IDD_MANILLA2DDEVTOOLSDISABLEM2DSETTINGS_DIALOG_WIDE		103
-#define IDC_STATIC_1			200
+#define IDD_M2D_DEVTOOLS_SETTINGS_DLG   101
+#define IDR_MAINFRAME                   128
+#define IDR_APPLY_CANCEL_MENU           129
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
