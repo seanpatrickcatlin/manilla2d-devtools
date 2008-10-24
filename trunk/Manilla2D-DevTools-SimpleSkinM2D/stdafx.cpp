0ee444b44a48ab7dc25bdf8e741f468536a7682c
catlin.sean <catlin.sean@45cd278c-9cb8-11dd-87e9-a30ea5446288>
2008-10-24 04:52:31 +0000

    this project has been canceled because it was a duplicate of my other project, M2DC, I just updated M2DC to fix its problems and include the functionality that this app was supposed to offer

--- trunk/Manilla2D-DevTools-SimpleSkinM2D/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Manilla2D-DevTools-SimpleSkinM2D.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
